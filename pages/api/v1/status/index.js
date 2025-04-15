ef2eaba42063365dc06c6f666918dbbe1fb24c10
LucasOlCunha <135914547+LucasOlCunha@users.noreply.github.com>
2025-04-15 22:54:14 +0000

    arruma o codigo

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -19,9 +19,7 @@ async function status(request, response) {
   });
 
   const databaseOpenedConnectionsValue =
-    databaseOpenedConnectionsResult.rows[0].count;  
-
-    
+    databaseOpenedConnectionsResult.rows[0].count;
 
   response.status(200).json({
     updated_at: updatedAt,
